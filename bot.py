e7d0aecb743d02887a91fc98d152b823282cc636
Briam Agudelo <bridamo.98@gmail.com>
2023-12-19 17:23:01 -0500

    add verbose

--- bot.py
+++ bot.py
@@ -2,21 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 url = 'https://www.365scores.com/es/news/premios-365-scores-mejor-jugador/'
-# Specify the path to the ChromeDriver executable
 chrome_driver_path = '/usr/bin/chromedriver'
-# Create a new instance of the Chrome driver
 service = webdriver.ChromeService(executable_path = chrome_driver_path)
 driver = webdriver.Chrome(service=service)
-# Open the URL in the Chrome browser
 driver.get(url)
 frame_selector = "//iframe[@src='https://www.riddle.com/embed/a/aXXyEyed?lazyImages=true&staticHeight=false']"
 radio_button_selector = "//h3[normalize-space()='Camilo MD']"
 check_selector = "//p[contains(text(),'¡Gracias por votar! A seguir con las siguientes ca')]"
-
-
-
+choose_selector = "//button[normalize-space()='Elegir']"
+checked = 1
 def click_radio_button():
 
     try:
@@ -25,46 +22,54 @@ def click_radio_button():
             EC.visibility_of_element_located((By.XPATH, frame_selector))
         )
 
-        print(frame)
+        print("- Frame loaded")
 
         driver.switch_to.frame(frame)
 
+        sleep(1)
 
         radio_button = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, radio_button_selector))
         )
 
-        print(radio_button)
+        sleep(1)
 
         radio_button.click()
-        radio_button.click()
 
-        print(f"Clicked on radio button with selector: {radio_button_selector}")
-
-        choose_selector = "//button[normalize-space()='Elegir']"
+        print("- Option selected")
 
         choose_button = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, choose_selector))
         )
 
-        print(choose_button)
+        sleep(1)
 
         choose_button.click()
 
-        WebDriverWait(driver, 5).until(
+        print("- Voting...")
+
+        WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, check_selector))
         )
 
+        print(f"- CHECKED: {checked}")
+        return 1
+
 
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"- ERROR: {e}")
+        print("- Continue...")
+        return 0
 
     finally:
         # Close the browser window
         driver.refresh()
+        print("- Refreshing...")
 
-# Example usage
-i = 1
-while i < 101:
-    click_radio_button()
+attempts = 0
+while attempts < 500:
+    i = click_radio_button()
+    checked+=i
+    attempts+=1
 
+driver.quit()
